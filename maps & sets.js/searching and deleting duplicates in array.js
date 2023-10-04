0551452c9c3cb60f612511be233df214c8814395
dennis <dennisjshofficial@gmail.com>
2023-10-04 12:18:22 +0530

    added code

--- maps & sets.js/searching and deleting duplicates in array.js
+++ maps & sets.js/searching and deleting duplicates in array.js
@@ -14,4 +14,18 @@ while(i<nums.length){
 
 console.log(j+1);
 
+// 2) using stacks...
+// let nums = [0,0,1,1,1,2,2,3,3,4];
+
+// let stack = [];
+
+// for(i = 0; i<nums.length; i++){
+//   if(stack[stack.length-1] === nums[i]){
+//     stack.pop();
+//   }
+//   stack.push(nums[i]);
+// }
+
+// console.log(stack);
+
 // youtube link:- https://www.youtube.com/watch?v=Fm_p9lJ4Z_8
