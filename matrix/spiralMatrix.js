f85353e444ff9bd69be06f5fc1507a7cd199dbc9
dennis <dennisjshofficial@gmail.com>
2023-10-07 09:40:27 +0530

    added code

--- matrix/spiralMatrix.js
+++ matrix/spiralMatrix.js
@@ -38,9 +38,10 @@ function printSpiral(a){
       }
     }
     
-    console.log(arr);
-  }
+    return (arr);
+}
   
-  let matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]];
-  
-  printSpiral(matrix);
+let matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]];
+
+let spiralMatrix = printSpiral(matrix);
+console.log(spiralMatrix);
