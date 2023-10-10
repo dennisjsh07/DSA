282698fcc4b64719136d5bb8e5662387bfd896c7
dennis <dennisjshofficial@gmail.com>
2023-10-10 10:50:19 +0530

    add code

--- maps & sets.js/two difference.js
+++ maps & sets.js/two difference.js
@@ -1,56 +1,42 @@
-// step-1 (SORT THE ARRAY).
-var key=5;
-var ans=[-1,-1];
-for(i=0;i<nums.length-1;i++)
-{
-  for(j=0;j<nums.length-1-i;j++)
-  {
-    if(nums[j+1]<nums[j])
-    {
-      var temp=nums[j];
-      nums[j]=nums[j+1];
-      nums[j+1]=temp;
+// brute solution... 
+// Tc = O(n);
+// Sc = O(1);
+function twoDiff(a){
+  a.sort();
+  
+  for(i = 0; i<a.length; i++){
+    for(j = i+1; j<a.length; j++){
+      if(a[j] - a[i] === target){
+        console.log(a[i],a[j]);
+      }
     }
   }
 }
-// STEP-2 INCREMENT i & j;
-console.log(nums);
-var i=0;
-var j=1;
-while(i<nums.length && j<nums.length)
-{
-  if(Math.abs(nums[j]-nums[i])==key)
-  {
-    ans[0]=nums[i];
-    ans[1]=nums[j];
-    break;
-  }
-  if(key<Math.abs(nums[j]-nums[i]))
-  {
-    j=j+1;
-  }
-  else
-  {
-    i=i+1;
-  }
-}
-console.log(ans);
 
-/// code link in youtube ///
-
-// normal method.
-var a=[5,2,3,80,5,20];
-target=60;
-var ans=[-1,-1];
-for(i=0;i<a.length;i++)
-{
-  for(j=i+1;j<a.length;j++)
-  {
-    if(Math.abs(a[j]-a[i])==target)
-    {
-      ans[0]=a[i];
-      ans[1]=a[j];
+// better solution... 
+// Tc = O(n);
+// Sc = O(n);
+function twoDiff(a){
+  let c = new Set();
+  
+  for(i = 0; i<a.length; i++){
+    c.add(a[i]);
+  }
+  
+  // console.log(c);
+  
+  for(i = 0; i<a.length; i++){
+    let b = a[i];
+    let d = Math.abs(target - b);
+    // console.log(d);
+    if(c.has(d)){
+      console.log(b ,d);
     }
   }
 }
-console.log(ans);
+
+var arr = [5,2,3,80,5,20];
+let target = 15;
+twoDiff(arr)
+
+
