d939b524a617e3697796fdb9758aa0e2a8d80ab8
dennis <dennisjshofficial@gmail.com>
2023-10-11 10:32:51 +0530

    added code

--- arrays.js/valid parenthess.js
+++ arrays.js/valid parenthess.js
@@ -1,31 +1,26 @@
-var s="()[]{}"
-var stack=[];
-for(var i=0;i<s.length;i++)
-{
-  if(stack.length==0)
-  {
-    stack.push(s[i]);
+function validParenthesis(a){
+  let stack = [];
+  
+  for(i = 0; i<a.length; i++){
+    if(stack.length === 0){
+      stack.push(a[i]);
+    }
+    else if(stack[stack.length-1] === '(' && a[i] === ')' ||
+    stack[stack.length-1] === '{' && a[i] === '}' ||
+    stack[stack.length-1] === '[' && a[i] === ']'){
+      stack.pop();
+    }
+    else{
+      stack.push(a[i]);
+    }
   }
-  else if((stack[stack.length-1]=="(" && s[i]==")")||
-  (stack[stack.length-1]=="{" && s[i]=="}")||
-  (stack[stack.length-1]=="[" && s[i]=="]"))
-  {
-    stack.pop();
-  }
-  else
-  {
-    stack.push(s[i]);
+  
+  if(stack.length === 0){
+    console.log(true);
+  } else{
+    console.log(false);
   }
 }
 
-if(stack.length==0) 
-{
-console.log(true);
-}
-else
-{
-console.log(false);
-}
-
-// time complexity =O(n);
-// space complexity =O(n);
+var s="()[){}"
+validParenthesis(s);
--- arrays.js/validate sequence.js
+++ arrays.js/validate sequence.js
@@ -1,24 +1,24 @@
-var pushed = [1,2,3,4,5];
-var popped = [4,5,3,2,1];
-var stack=[];
-var j=0;
-for(i=0;i<pushed.length;i++)
-{
-  stack.push(pushed[i]);
-  while(stack[stack.length-1]==popped[j] && stack.length!=0)
-  {
-    stack.pop();
-    j++;
+function validateSequence(a,b){
+  
+  let stack = [];
+  let j = 0;
+  
+  for(i = 0; i<pushed.length; i++){
+    stack.push(a[i]);
+    while(stack[stack.length-1] === b[j] && j<popped.length){
+      stack.pop();
+      j++
+    }
+  }
+  
+  if(stack.length > 0){
+    console.log(false);
+  } else{
+    console.log(true);
   }
 }
-if(stack.length==0)
-{
-  console.log("valid");
-}
-else
-{
-  console.log("invalid");
-}
 
-// time complexity =O(n);
-// space complexity =O(n);
+var pushed = [1,2,3,4,5];
+var popped = [3,5,3,2,2];
+
+validateSequence(pushed,popped);
