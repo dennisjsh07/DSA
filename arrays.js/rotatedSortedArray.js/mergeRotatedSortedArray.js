9e448c7df403497ee7107fc0bc4e2e954e3842bf
dennis <dennisjshofficial@gmail.com>
2023-10-25 10:19:06 +0530

    added code

--- arrays.js/rotatedSortedArray.js/mergeRotatedSortedArray.js
+++ arrays.js/rotatedSortedArray.js/mergeRotatedSortedArray.js
@@ -1,3 +1,4 @@
+// better solution... (TC = O(n), SC = O(n))
 function mergeSortedArray(){
     let ans = [];
     let i = 0;
@@ -33,6 +34,39 @@ function mergeSortedArray(){
 
     return ans;
 };
+
+// optimal solution... (TC = O(n), SC = O(1))
+function mergeSortedArray(){
+    let i = m-1;
+    let j = n-1;
+    let k = m+n-1;
+    
+    while(i >= 0 && j >=0){
+      if(nums1[i] >= nums2[j]){
+        nums1[k] = nums1[i];
+        i--;
+      } else{
+        nums1[k] = nums2[j];
+        j--;
+      }
+      k--;
+    }
+    
+    while(i >= 0){
+      nums1[k] = nums1[i];
+      i--;
+      k--;
+    }
+    
+    while(j >= 0){
+      nums1[k] = nums2[j];
+      j--;
+      k--
+    }
+    
+    return nums1;
+};
+  
 
 let nums1 = [1,2,3,0,0,0]; 
 let m = 3; 
