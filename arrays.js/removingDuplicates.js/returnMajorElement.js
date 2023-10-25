c172ab7d1dce8cdf80191a5d3e5161484a333cf7
dennis <dennisjshofficial@gmail.com>
2023-10-25 13:15:49 +0530

    added cde

---
+++ arrays.js/removingDuplicates.js/returnMajorElement.js
@@ -0,0 +1,27 @@
+function majorityElement(){
+  
+    let a = new Map()
+  
+    for(let i = 0; i<nums.length; i++){
+      if(a.get(nums[i]) === undefined){
+        a.set(nums[i], 1);
+      } else{
+        a.set(nums[i], a.get(nums[i])+1);
+      }
+    }
+    
+    let max = 0;
+    index = 0;
+    for(let i = 0; i<nums.length; i++){
+      console.log(nums[i]+'-->'+a.get(nums[i]))
+      if(a.get(nums[i]) > max){
+        max = a.get(nums[i]);
+        index = i;
+      }
+    }
+    
+    console.log(nums[index]);
+  }
+  
+  let nums = [3,3,4];
+  majorityElement();
